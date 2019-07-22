0ffb9e15e9ad76ad6581bcb15b94cbf35a4e6d7d
Sindy Tatiana Fuentes Gutierrez <sindy.fuentes@Quipux.local>
2019-07-22 17:34:21 -0500

    Se crea funcionalidad para devolver servicios del spa por id

--- NodeJS Projects/SpaFeliz-api/SpaFeliz-api/controllers/servicesController.js
+++ NodeJS Projects/SpaFeliz-api/SpaFeliz-api/controllers/servicesController.js
@@ -10,6 +10,10 @@ exports.services_all_get = function (req, res){
 }
 
 exports.services_detail_get = function (req, res){
-    res.send ('devolver todos los servicios del con id ' + req.params.id);
+    serviceModel.findAll({
+        attributes:['id', 'name', 'image', 'happy'], where: {id: req.params.id}
+    })
+    .then(data => res.json(data))
+    .catch (err => res.status(500).send(err))
 }
 
